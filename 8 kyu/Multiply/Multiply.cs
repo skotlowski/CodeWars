437aa83f02904879b7372da001a46911f5f8ae68
skotlowski <szymonkotlowski20@gmail.com>
2019-10-15 22:12:26 +0200

    CodeWars exercises, daily update

---
+++ 8 kyu/Multiply/Multiply.cs
@@ -0,0 +1,10 @@
+//link to exercise https://www.codewars.com/kata/50654ddff44f800200000004
+
+public class CustomMath {
+
+    public static int multiply(int a, int b) 
+    {
+        return a*b;
+    }
+    
+}
